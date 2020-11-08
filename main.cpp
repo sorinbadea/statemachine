8a3d1133e561fa8d883addd6d6862b3deec50994
Sorin <sorin.badea@gmail.com>
2020-11-08 16:36:46 +0100

    - improve the usage of the test machine

--- main.cpp
+++ main.cpp
@@ -4,9 +4,43 @@
 #include "results.h"
 #include "results.cpp"
 
+/**
+ * run each state_machine until the end
+ * compare the expected results wit the returned
+ * \@param engine   test engine (state_machine)
+ * \@param expected expected results
+ */
+template< typename T, typename TResult >
+void run_tests(state_machine<T, TResult>& engine, std::list<TResult> expected)
+{
+   TestSuiteDone stop = TestSuiteDone::NO;
+   while(stop == TestSuiteDone::NO)
+   {
+       /** 
+        * non blocking call regardless 
+        * of test behaviour */
+       stop = engine.step();
+    }
+
+    std::list< TResult > results = engine.get_results();
+    assert( expected.size() == results.size() );
+
+    /** expected and results list iterators */
+    typename std::list< TResult >::iterator it_r = results.begin();
+    typename std::list< TResult >::iterator it_e = expected.begin();
+
+    /**
+     * check the expected and the returned results 
+     * show statistics */
+    Results<TResult> stat;
+    for(; it_r != results.end(); it_r++, it_e++ )
+    {
+        stat.compare(*it_e, *it_r);
+    }
+}
+
+
 int main(void) {
-
-
     {
         /** test class1 instanciation */
         test_class1 foo;
@@ -21,31 +55,11 @@ int main(void) {
         engine.set_step(&test_class1::test2);
         engine.set_step(&test_class1::test3);
     
-        TestSuiteDone stop = TestSuiteDone::NO;
-        while(stop == TestSuiteDone::NO)
-        {
-            /** 
-	    * non blocking call regardless 
-	    * of test behaviour */
-            stop = engine.step();
-        }
-
-        /**
-        * results display
-        */
-        {
-            Results<int> r;
-            std::list<int> results = engine.get_results();
-            std::list<int>::iterator it = results.begin();
-    
-            r.compare(1, *it++);
-            r.compare(1, *it++);
-            r.compare(1, *it);
-        }
+	std::list<int> expected = {1, 1, 1};
+	run_tests<test_class1, int>( engine, expected);
     }
 
     {
-
         /** test class2 instanciation 
 	 * methods returning std::string */
         test_class2 foo;
@@ -55,28 +69,9 @@ int main(void) {
         engine.set_step(&test_class2::test1);
         engine.set_step(&test_class2::test2);
         engine.set_step(&test_class2::test3);
-    
-        TestSuiteDone stop = TestSuiteDone::NO;
-        while(stop == TestSuiteDone::NO)
-        {
-            /** 
-	    * non blocking call regardless 
-	    * of test behaviour */
-            stop = engine.step();
-        }
 
-        /**
-        * results display
-        */
-        {
-            Results< std::string > r;
-            std::list< std::string > results = engine.get_results();
-            std::list< std::string >::iterator it = results.begin();
-    
-            r.compare("s1", *it++);
-            r.compare("s2", *it++);
-            r.compare("s3", *it);
-        }
+	std::list<std::string> expected = {"s1", "s2", "s33"};
+	run_tests<test_class2, std::string>( engine, expected);
     }
     
     return 0;
--- readme.txt
+++ readme.txt
@@ -10,8 +10,8 @@ The core of the prototype is the state_machine.h
 It takes a class instance and pointers to it's methods
 and calls each of them without blocking. 
 In the same time preserves the order of the calls.
-for the moment the supported method signature is
-int some_method(void);
+
+The supported method signature is <type> some_method(void);
 
 For usage see the file main.cpp, in this case
 the test1, test2, test3 methods are called in this order.
